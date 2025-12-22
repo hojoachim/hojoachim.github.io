87427f2e16977c3c2667c303f942d29d5404c28e
hojoachim <95039268+hojoachim@users.noreply.github.com>
2025-12-22 22:37:23 +0900

    Add files via upload
    - added touch screen functionality for Frog/Oliver picture change

--- js/script_about.js
+++ js/script_about.js
@@ -8,13 +8,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const frogText = document.querySelector('.frog-text');
     const oliverText = document.querySelector('.oliver-text');
 
-    imgContainer.addEventListener('mouseenter', () => {
+    imgContainer.addEventListener('mouseenter', showOliver);
+    imgContainer.addEventListener('touchstart', showOliver);
+
+    imgContainer.addEventListener('mouseleave', showFrog);
+    imgContainer.addEventListener('touchend', showFrog);
+
+    function showOliver(e) {
         frogText.classList.add('hide');
         oliverText.classList.add('show');
-    });
+    }
 
-    imgContainer.addEventListener('mouseleave', () => {
+    function showFrog(e) {
         frogText.classList.remove('hide');
         oliverText.classList.remove('show');
-    });
+    }
 });
