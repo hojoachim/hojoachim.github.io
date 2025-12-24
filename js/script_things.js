df86e3b49e169337cbaf0425a5c864185ca1f850
hojoachim <95039268+hojoachim@users.noreply.github.com>
2025-12-24 15:44:49 +0900

    Add files via upload
    - fixed mobile functionality

--- js/script_things.js
+++ js/script_things.js
@@ -7,17 +7,32 @@ document.addEventListener("DOMContentLoaded", () => {
     let spinBtn = document.querySelector('.spin-btn');
     let segments = document.querySelectorAll('.segment');
     let myModal = new bootstrap.Modal(document.getElementById('myModal'));
-
+    
     // Open Bootstrap modal on segment click
+    function showModal(segment) {
+        myModal.show();
+        document.getElementById('modalHeader').textContent = segment.getAttribute('data-header');
+        document.getElementById('modalBody').textContent = segment.getAttribute('data-body');
+        document.getElementById('modalImg').src = segment.getAttribute('data-image');
+    }
+    
     segments.forEach(segment => {
-        segment.onclick = function(e) {
+        let segmentsTap = false
+        // Mobile: Tap on segment
+        segment.addEventListener('touchstart', function(e) {
+            segmentsTap = true;
             e.preventDefault(); // Prevent default action of tap on mobile devices
-            myModal.show();
-            document.getElementById('modalHeader').textContent = segment.getAttribute('data-header');
-            document.getElementById('modalBody').textContent = segment.getAttribute('data-body');
-            document.getElementById('modalImg').src = segment.getAttribute('data-image');
-            // console.log(document.getElementById('modalImg'));
-        }
+            showModal(segment);
+        }, {passive: false});
+
+        // Desktop: Click on segment
+        segment.addEventListener('click', function(e) {
+            if (segmentsTap) {
+                segmentsTap = false; // Reset mobile tap flag
+                return; // Ignore the click event if it was a mobile tap
+            }   
+            showModal(segment);
+        });
     });
 
     // Script for spinning wheel
@@ -28,9 +43,10 @@ document.addEventListener("DOMContentLoaded", () => {
     // Initial random value for spin between 1000 and 3600 deg adjusted to always stop in the middle of a segment
     let value = -(Math.floor((Math.ceil(Math.random() * 2600) + 1000) / segmentAngle) * segmentAngle - 45);
     // console.log(value);
-    spinBtn.onclick = function(e) {
-        e.preventDefault(); // Prevent default action of tap on mobile devices
-        // console.log(value);
+    let spinBtnTap = false; // Flag to track mobile tap state
+
+    // Spin the wheel
+    function spinWheel (e) {
         wheel.style.transform = "rotate(" + value + "deg)";
         // Segment angle after spin ends
         let finalValue = value;
@@ -59,5 +75,21 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById('modalImg').src = selectedSegment.getAttribute('data-image');
         }, 3000); // match transition duration
     }
-    
+
+    // Mobile: Tap on spin button
+    spinBtn.addEventListener('touchstart', function(e) {
+        spinBtnTap = true;
+        e.preventDefault(); // Prevent default action of tap on mobile devices
+        spinWheel(e);
+    }, {passive: false});
+
+
+    // Desktop: Click on spin button
+    spinBtn.addEventListener('click', function(e) {
+        if (spinBtnTap) {
+            spinBtnTap = false; // Reset mobile tap flag
+            return; // Ignore the click event if it was a mobile tap
+        }
+        spinWheel(e);
+    });
 });
